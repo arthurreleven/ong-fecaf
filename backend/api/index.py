6dcc2cbe7db7c51d7408e7565e0cd5232cf089d1
arthurreleven <yarthur1132@gmail.com>
2026-05-15 21:44:33 -0300

    remove cors

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,7 +1,3 @@
-import traceback
-try:
-    from app import create_app
-    app = create_app()
-except Exception as e:
-    print(traceback.format_exc())
-    raise
+from app import create_app
+
+app = create_app()
